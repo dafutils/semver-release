009f4fe6d11e45b933dd22c9bec75a7b9349b893
EmilDafinov <emil.dafinov@appdirect.com>
2018-01-05 09:16:01 -0500

    simplify call to semver-tool

--- semverTag.sh
+++ semverTag.sh
@@ -1,19 +1,20 @@
 #!/bin/bash
 
-set -e -u
-shopt -s nocasematch				
+set -e -u			
 
 rm -rf ./semver-tool
 git clone https://github.com/fsaintjacques/semver-tool.git
 
+echo "Attempting to resolve the latest release version tag..."
 LAST_RELEASE_TAG=$(git describe --abbrev=0 --tags)
+echo "Previous release verion tag: ${LAST_RELEASE_TAG}"
 LAST_RELEASED_VERSION=${LAST_RELEASE_TAG#v}
+echo "Previous release verion number: ${LAST_RELEASED_VERSION}"
 
-case "$1" in
-	"MAJOR" ) RELEASE_VERSION=$(./semver-tool/src/semver bump major ${LAST_RELEASED_VERSION}) ;;
-	"MINOR" ) RELEASE_VERSION=$(./semver-tool/src/semver bump minor ${LAST_RELEASED_VERSION}) ;;
-	"PATCH" ) RELEASE_VERSION=$(./semver-tool/src/semver bump patch ${LAST_RELEASED_VERSION}) ;;
-esac
+RELEASE_TYPE=$(echo "$1" | tr '[:upper:]' '[:lower:]')
+echo "Release type is: ${RELEASE_TYPE}"
+
+RELEASE_VERSION=$(./semver-tool/src/semver bump ${RELEASE_TYPE} ${LAST_RELEASED_VERSION})
 
 echo "The released version is: [${RELEASE_VERSION}]"
 # git tag -a "v${RELEASE_VERSION}" -m "sbt-dynver compatible tag for ${RELEASE_VERSION}"
